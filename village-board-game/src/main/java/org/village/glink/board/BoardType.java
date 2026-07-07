a6317a74bd54196e27b5adb6b4e95accccfc49cf
yepeijie <yepeijie@qtict.com>
2026-07-07 17:18:25 +0800

    1

--- village-board-game/src/main/java/org/village/glink/board/BoardType.java
+++ village-board-game/src/main/java/org/village/glink/board/BoardType.java
@@ -1,16 +1,29 @@
 package org.village.glink.board;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 /**
  * @author yepeijie
  * @date 2026/7/3
  */
+@Getter
+@AllArgsConstructor
 public enum BoardType {
-    TAG,
-    ATTRIBUTE,
-    STATE,
-    SKILL,
-    PERK,
+    TAG(Group.DATA),
+    ATTRIBUTE(Group.DATA),
+    STATE(Group.DATA),
+    SKILL(Group.DATA),
+    PERK(Group.DATA),
     //
+    ITEM(Group.INSTANCE),
+
+    ;
+    private final Group group;
 
 
+    enum Group {
+        DATA,
+        INSTANCE
+    }
 }
