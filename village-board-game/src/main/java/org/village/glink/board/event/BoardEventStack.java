45fc9f6882fb02c19814ad96c03d57fa9089c4f7
yepeijie <yepeijie@qtict.com>
2026-08-03 11:40:35 +0800

    1

--- village-board-game/src/main/java/org/village/glink/board/event/BoardEventStack.java
+++ village-board-game/src/main/java/org/village/glink/board/event/BoardEventStack.java
@@ -8,24 +8,40 @@ import org.village.lite.common.util.collection.StackArray;
  * @date 2026/7/28
  */
 public class BoardEventStack {
+    protected static final int MAX_SIZE = 1024;
     private final BoardWorld world;
-    private final StackArray<BoardEvent> eventStack;
+    private final StackArray<BoardEvent> events;
 
     public BoardEventStack(BoardWorld world) {
         this.world = world;
-        this.eventStack = new StackArray<>(10, 5);
+        this.events = new StackArray<>(5, 5);
     }
 
     public BoardEvent add(BoardEventData data) {
-        final int eventIdx = eventStack.size();
+        final int eventIdx = events.size();
+        if (eventIdx >= MAX_SIZE) {
+            throw new StackOverflowError();
+        }
         final long eventTime = world.currentTime();
         BoardEvent event = new BoardEvent(world, eventTime, eventIdx, data);
-        eventStack.add(event);
+        events.add(event);
         return event;
     }
 
-    public int eventSize() {
-        return eventStack.size();
+    public int size() {
+        return events.size();
+    }
+
+    public BoardEvent last() {
+        return events.last();
+    }
+
+    public BoardEvent poll() {
+        return events.poll();
+    }
+
+    public BoardEvent get(int index) {
+        return events.get(index);
     }
 }
 
